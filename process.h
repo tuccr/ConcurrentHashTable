3151a7ac56d9ab0077118d52fbdd2f1ca05bc8c3
tuccr <tuckerbcarroll.business@gmail.com>
2025-11-21 16:34:05 -0500

    insert function with waiting and signaling

--- process.h
+++ process.h
@@ -1,21 +1,32 @@
 #include "hashtable.h"
 #include "command.h"
 #include "logging.h"
-#include <pthread.h>
+#include "threads.h"
+
+pthread_mutex_t read_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t write_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+
+wait_queue_t wait_queue;
 
 /*
 Accepts command_t struct pointer and inserts into hashtable.
 */
 void* insert(void* arg) {
-    // cv.wait()
-
-    // cv.signal()
-
     command_t* cmd = (command_t*)arg;
+    
+    log_event(WAIT, cmd->priority);
 
     hashRecord * record = newHashRecord((uint8_t *)(cmd->name), (uint32_t)(cmd->salary));
 
-    // write_lock()
+    pthread_mutex_lock(&write_mutex);
+    while(get_highest_priority(&wait_queue) != cmd->priority) {
+        pthread_cond_wait(&cv, &write_mutex);
+    }
+    log_event(AWAKENED, cmd->priority);
+    log_cmd(cmd);
+    log_event(WRITE_LOCK_ACQUIRE, cmd->priority);
+
 
     if(htp->head == NULL) {
         htp->head = record;
@@ -23,12 +34,23 @@ void* insert(void* arg) {
 
     hashRecord* current = htp->head;
     while(current->next != NULL) {
+        if(current->hash == record->hash && strcmp(current->name, record->name) == 0) {
+            freeHashRecord(record);
+            
+            printf("Insert failed: duplicate entry for %s\n", record->name);
+            
+            pthread_mutex_unlock(&write_mutex);
+            log_event(WRITE_LOCK_RELEASE, cmd->priority);
+            return NULL;
+        }
         current = current->next;
     }
     current->next = record;
     htp->size += 1;
 
     // write_unlock()
+    pthread_mutex_unlock(&write_mutex);
+    log_event(WRITE_LOCK_RELEASE, cmd->priority);
 
     return NULL;
 
@@ -66,23 +88,32 @@ void* search(void* arg) {
 Accepts command_t struct pointer and updates salary in hashtable entry if found.
 */
 void* update(void* arg) {
-    // cv.wait()
-
-    // cv.signal()
     command_t* cmd = (command_t*)arg;
 
-    uint32_t hash = jenkins_one_at_a_time_hash((uint8_t *)(cmd->name), strlen(cmd->name));
+    pthread_mutex_lock(&read_mutex);
+    while(get_highest_priority(&wait_queue) != cmd->priority) {
+        pthread_cond_wait(&cv, &read_mutex);
+    }
+    log_event(AWAKENED, cmd->priority);
+    log_cmd(cmd);
+    log_event(READ_LOCK_ACQUIRE, cmd->priority);
+    pthread_cond_signal(&cv);
 
+    uint32_t hash = jenkins_one_at_a_time_hash((uint8_t *)(cmd->name), strlen(cmd->name));
     // read_lock()
+    log_event(READ_LOCK_ACQUIRE, cmd->priority);
     hashRecord* toChange = (hashRecord*)search(arg);
     // read_unlock()
+    log_event(READ_LOCK_RELEASE, cmd->priority);
 
     // write_lock()
+    log_event(WRITE_LOCK_ACQUIRE, cmd->priority);
     if(toChange != NULL) {
         toChange->salary = (uint32_t)(cmd->salary);
     }
 
     // write_unlock()
+    log_event(WRITE_LOCK_RELEASE, cmd->priority);
 
     return NULL;
 }
@@ -118,6 +149,8 @@ void* delete(void* arg) {
         prev = current;
         current = current->next;
     }
+
+
 
     // read_unlock()
 
